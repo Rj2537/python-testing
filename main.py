59ff10b3cbed3b2e646af85df04aed7e6c3b223f
The one and the only RJ <rjgamestar777@gmail.com>
2025-02-01 21:46:15 +0530

    Trials for today

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+import requests as r
+import json 
+file_path ='url.json'
+try:
+    with open(file_path, "r") as file:
+        data = json.load(file)
+        url = data['default'][0]
+    # print(url)
+except FileNotFoundError:
+    print(f"Error: The file '{file_path}' was not found.")
+except json.JSONDecodeError:
+    print(f"Error: The file '{file_path}' contains invalid JSON.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+post_url = url+"/meta/anilist/watch/spy-x-family-episode-1"
+response = r.get(post_url)
+data = response.json()
+ 
+print(data)
